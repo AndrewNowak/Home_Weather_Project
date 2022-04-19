c297602cb2e0f83ae76da8066d2c713d40bc6761
Andrew Nowak <anowak1597@yahoo.com>
2022-04-19 10:12:43 -0400

    Update Weather_Station_Software_V1.py
    Most recent updates

--- Weather_Station_Software_V1.py
+++ Weather_Station_Software_V1.py
@@ -1,55 +1,53 @@
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import board
 import adafruit_bme680
 import time
 
-#Set up GPIO pins for anemometer sensor
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12,GPIO.IN)
+#set up GPIO pins for anemometer sensor
+GPIO.setmode(GPIO.BCM) # Do not use board, this will not work with I2C (???)
+GPIO.setup(12,GPIO.IN) # Becuase BCM, this is pin 32 (GPIO 12), not pin 12
 
-#Set up so the sensor can read tp Pi
+#set up so sensor can read 
 i2c = board.I2C()
 sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
-vane_radius = 5 #cm
+vane_radius = 0.05 #m
 vane_circum = 2*3.14159*vane_radius
 efficiency = 1 #adjust through testing
 
 try:
     while True:
-        #Read values from BME 680
-        fahr = sensor.temperature*1.8 + 32 #sensor reads in Celsius, convert to Fahrenheit
-        temp = fahr
+        fahr = sensor.temperature*1.8 +32 #C to F
+        temp=fahr
         voc = sensor.gas
         humid = sensor.humidity
         pres = sensor.pressure
-
+        
         sensor_prev = GPIO.input(12)
+        #print('sensor_prev'.format(sensor_prev))
         time_delay = 20
-        anemom_read_time = time.time() + time_delay # seconds time delay
-        cnt = 0 # Number of revolutions over time interval
+        anemom_read_time = time.time() + time_delay
+        cnt= 0
         while time.time() < anemom_read_time:
             sensor_read = GPIO.input(12)
+            #print(sensor_read)
             if sensor_read == 1 and sensor_prev == 0:
-                cnt = cnt + 1
+                cnt = cnt+1
             sensor_prev = sensor_read
             time.sleep(0.001)
-        
         rps = cnt/time_delay
-        wind_speed =  rps*vane_circum*efficiency*2.23694 #mps -> mph
+        wind_speed = rps*vane_circum*efficiency*2.23694
         
-
         print('Temperature: {} degrees F'.format(fahr))
-        print('Gas: {} ohms'. format(voc))
+        print('Gas: {} ohms'.format(voc))
         print('Humidity: {} %'.format(humid))
         print('Pressure: {} hpa'.format(pres))
-        print(' Wind Speed: {} mph'.format(wind_speed))
-
-        time.sleep(580) # wait almost 10 minutes for another reading 
+        print('Wind Speed: {} mph'.format(wind_speed))
         
-    
-
-
+        time.sleep(30)
+finally:
+    print('error')
+    GPIO.cleanup()
 
 
 
